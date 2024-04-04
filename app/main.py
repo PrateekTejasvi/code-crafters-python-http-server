98c3f2e7955d187b55193d99e273d2feca4cc48e
Prateek Tejasvi <prateektejasvi@pop-os.localdomain>
2024-04-04 14:59:58 +0530

    status 200 ok

--- app/main.py
+++ app/main.py
@@ -8,6 +8,21 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept() # wait for client
+    print("listening on port 4221:")
+    conn,addr = server_socket.accept()
+
+    while conn:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+            conn.sendall(data)
+
+
+
+
+
 
 
 if __name__ == "__main__":
