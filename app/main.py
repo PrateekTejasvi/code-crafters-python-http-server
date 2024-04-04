9e519365201b68821333822db7d1b42c4814b6d9
Prateek Tejasvi <prateektejasvi@pop-os.localdomain>
2024-04-04 15:06:51 +0530

    status 200 -test5

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
     print("listening on port 4221:")
     conn,addr = server_socket.accept()
 
