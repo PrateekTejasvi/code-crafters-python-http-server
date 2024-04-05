5b9231358a21cb51af5b17f4c4d9033aafdb9014
Prateek Tejasvi <prateektejasvi@pop-os.localdomain>
2024-04-05 10:59:06 +0530

    need to refactor code

--- app/main.py
+++ app/main.py
@@ -7,18 +7,20 @@ class decodeData(object):
         request = request.decode()
         lines = request.split('\r\n')
         method , path,reponse= lines[0].split()
+        userAgent = lines[2]
         self.method = method 
         self.path = path 
         self.response = reponse
+        self.userAgent = userAgent
 
-
+    
 
 def main():
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     print("listening on port 4221:")
+
+
 
     while True:
 
@@ -31,14 +33,16 @@ def main():
                 response = f"HTTP/1.1 200 OK\r\n\r\n"
             elif request.path.startswith("/echo/"):
                 echo_data = request.path[6:]
-                print(echo_data)
                 response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo_data)}\r\n\r\n{echo_data}"
+            elif request.path.startswith("/user-agent"):
+                parsedData = request.userAgent.strip().split(": ")[1]
+                print(parsedData)
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(parsedData)}\r\n\r\n{parsedData}"
             else:
                 response = f"HTTP/1.1 404 NOT FOUND\r\n\r\n" 
             
             conn.send(response.encode('utf-8'))
             
-
 
         if not data:
             break  
