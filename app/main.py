9698a6177bcf7ba05db0d2f46bcfda1193da53e6
Prateek Tejasvi <prateektejasvi@pop-os.localdomain>
2024-04-06 12:22:52 +0530

    post a file

--- app/main.py
+++ app/main.py
@@ -18,6 +18,15 @@ class decodeData(object):
         self.response = reponse
         self.userAgent = userAgent
 
+def postFile(path,request):
+    #get the file contents 
+    decoded = request.decode()
+    lines = decoded.split('\r\n')
+    content = lines[len(lines)-1]
+    with open(path,"w") as f: 
+        f.write(content)
+    response = "HTTP/1.1 201 OK\r\nContent-length: 0\r\n\r\n".encode()
+    return response
 
 def getDir():
     args = sys.argv 
@@ -28,12 +37,15 @@ def getDir():
     return dir
 
 
+
+
+
 def handleConnections(conn):
     HTTPOK =  f"HTTP/1.1 200 OK\r\n\r\n".encode('utf-8')
     def sendValidResponse(data,type):
         send_resp = f"HTTP/1.1 200 OK\r\nContent-Type: {type}\r\nContent-Length: {len(data)}\r\n\r\n{data}".encode('utf-8')
         return send_resp
-    def postFile(dir):
+    def GetFile(dir):
         response = b"HTTP/1.1 404 NOT FOUND\r\n\r\n"
         if os.path.exists(dir):
             with open(dir) as f:
@@ -44,6 +56,7 @@ def handleConnections(conn):
     with conn:
         data = conn.recv(1024)
         request = decodeData(data)
+        print(request.method)
         if request.path == '/':
             response = HTTPOK
         elif request.path.startswith("/echo/"):
@@ -52,11 +65,17 @@ def handleConnections(conn):
         elif request.path.startswith("/user-agent"):
             parsedData = request.userAgent.strip().split(": ")[1]
             response = sendValidResponse(parsedData,"text/plain")
-        elif request.path.startswith("/file"):
+        elif request.path.startswith("/file") and request.method == "GET":
             file_name = request.path[7:]
             dir = getDir()
             path = f"{dir[:len(dir)-1]}/{file_name}"
-            response= postFile(path)
+            response= GetFile(path)
+        elif request.path.startswith("/file") and request.method=="POST":
+            file_name = request.path[7:]
+            dir = getDir()
+            print(file_name)
+            path = f"{dir[:len(dir)-1]}/{file_name}"
+            response = postFile(path,data)
         else:
             response = f"HTTP/1.1 404 NOT FOUND\r\n\r\n".encode('utf-8')
         conn.send(response)
