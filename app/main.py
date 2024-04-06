4f6b4aff0e8a583f3d2b1d07de1d39a1bef3bc25
Prateek Tejasvi <prateektejasvi@pop-os.localdomain>
2024-04-06 10:46:12 +0530

    get a file

--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
 # Uncomment this to pass the first stage
 import socket
 import threading
+import os
+import sys
 
 server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 print("listening on port 4221:")        
-
-
-
-
 
 class decodeData(object): 
     def __init__(self,request): 
@@ -21,34 +19,52 @@ class decodeData(object):
         self.userAgent = userAgent
 
 
+def getDir():
+    args = sys.argv 
+    dir = ""
+    for i in range(len(args)):
+        if args[i] == "--directory":
+            dir = args[i+1]
+    return dir
+
 
 def handleConnections(conn):
     HTTPOK =  f"HTTP/1.1 200 OK\r\n\r\n".encode('utf-8')
-    def sendValidResponse(data):
-        send_resp = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(data)}\r\n\r\n{data}".encode('utf-8')
+    def sendValidResponse(data,type):
+        send_resp = f"HTTP/1.1 200 OK\r\nContent-Type: {type}\r\nContent-Length: {len(data)}\r\n\r\n{data}".encode('utf-8')
         return send_resp
+    def postFile(dir):
+        response = b"HTTP/1.1 404 NOT FOUND\r\n\r\n"
+        if os.path.exists(dir):
+            with open(dir) as f:
+                content = f.read()
+                response=sendValidResponse(content,"application/octet-stream")
+        return response
 
     with conn:
-        while True:
-            data = conn.recv(1024)
-            request = decodeData(data)
-            if request.path == '/':
-                response = HTTPOK
-            elif request.path.startswith("/echo/"):
-                echo_data = request.path[6:]
-                response = sendValidResponse(echo_data)
-            elif request.path.startswith("/user-agent"):
-                parsedData = request.userAgent.strip().split(": ")[1]
-                response = sendValidResponse(parsedData)
-            else:
-                response = f"HTTP/1.1 404 NOT FOUND\r\n\r\n".encode('utf-8')
-            conn.send(response)
+        data = conn.recv(1024)
+        request = decodeData(data)
+        if request.path == '/':
+            response = HTTPOK
+        elif request.path.startswith("/echo/"):
+            echo_data = request.path[6:]
+            response = sendValidResponse(echo_data,"text/plain")
+        elif request.path.startswith("/user-agent"):
+            parsedData = request.userAgent.strip().split(": ")[1]
+            response = sendValidResponse(parsedData,"text/plain")
+        elif request.path.startswith("/file"):
+            file_name = request.path[7:]
+            dir = getDir()
+            path = f"{dir[:len(dir)-1]}/{file_name}"
+            response= postFile(path)
+        else:
+            response = f"HTTP/1.1 404 NOT FOUND\r\n\r\n".encode('utf-8')
+        conn.send(response)
 
 def main():
     try: 
         while True:
             conn, addr = server_socket.accept()  # wait for client
-            print(f"Connected to: {addr}")
             client_thread = threading.Thread(target=handleConnections, args=(conn,))
             client_thread.start()
     finally:
